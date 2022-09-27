f98db31c0df6c4fd85a56458a680f9e84fd587bc
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-27 11:10:39 +0200

    added concat function on my first app

---
+++ app/concat.js
@@ -0,0 +1,7 @@
+const _ = require("lodash")
+
+function concat(array1, array2) {
+    return _.concat(array1,array2)
+}
+
+module.exports.concat = concat
--- app/index.js
+++ app/index.js
@@ -1,8 +1,14 @@
 
 const sum = require("./sum.js")
+const concat = require("./concat.js")
 
 const AddNumber= [1,2,3,4,5]
+array1 = [1,2,3,4,5]
+array2 = [1,2,3,4,5]
 
 const result = sum.sum(AddNumber)
+const result2 = concat.concat(array1, array2)
 
 console.log(`Total sum of array : ${result}`)
+
+console.log(`The result of the sum is ${result2}`)
