c5967e05ccedf3542c7b68a2940fb29a31f057e9
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-27 12:14:34 +0200

    added read external input file

--- app/index.js
+++ app/index.js
@@ -1,14 +1,18 @@
 
 const sum = require("./sum.js")
 const concat = require("./concat.js")
+const fs = require('fs');
 
 array1 = [1,2,3,4,5]
 array2 = [1,2,3,4,5]
 
-jsonin = {
-    "array1":[1,2],
-    "array2":[3,4],
-}
+// jsonin = {
+//     "array1":[1,2],
+//     "array2":[3,4],
+// }
+const data = fs.readFileSync('./app/input.json', (err,data) => {});
+const jsonin = JSON.parse(data);
+// console.log(`prova`,dataParsed);
 
 const arraytot = jsonin.array1.concat(jsonin.array2)
 const result_concat = concat.concat(jsonin.array1, jsonin.array2)
