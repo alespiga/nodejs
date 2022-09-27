efcb3debe9a86de7bd85f631feb0960ee88efb6c
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-27 16:22:48 +0200

    used then & catch handlers

--- asynchronous/ice-cream-promise/ice_cream.js
+++ asynchronous/ice-cream-promise/ice_cream.js
@@ -4,7 +4,7 @@ let stocks = {
   holder: ["cone", "cup", "stick"],
   toppings: ["chocolate", "peanuts"],
 };
-let is_shop_open = true;
+let is_shop_open = false;
 
 let order = (time, work) => {
   return new Promise((resolve, reject) => {
@@ -18,4 +18,32 @@ let order = (time, work) => {
   });
 };
 
-order(2000, () => console.log(`${stocks.Fruits[0]} was selected`));
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
+  .then(() => {
+    return order(0000, () => console.log("production has started"));
+  })
+  .then(() => {
+    return order(2000, () => console.log("Fruit has been chopped"));
+  })
+  .then(() => {
+    return order(1000, () =>
+      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} added`)
+    );
+  })
+  .then(() => {
+    return order(1000, () => console.log("start the machine"));
+  })
+  .then(() => {
+    return order(2000, () =>
+      console.log(`ice cream placed on ${stocks.holder[1]}`)
+    );
+  })
+  .then(() => {
+    return order(3000, () => console.log(`${stocks.toppings[0]} as toppings`));
+  })
+  .then(() => {
+    return order(2000, () => console.log("Serve Ice Cream"));
+  })
+  .catch(() => {
+    console.log("Customer left");
+  });
--- asynchronous/index.js
+++ asynchronous/index.js
@@ -1,4 +1,4 @@
 console.log("ASYNC TEST STARTING...");
 // require("./callback.js")
 require("../asynchronous/ice-cream-promise/ice_cream.js");
-console.log("ASYNC TEST END");
+// console.log("ASYNC TEST END");
