d3516f9dc91c0f5d89a6563b92d8864907d434c4
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 10:29:15 +0200

    added swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 // Setup
 const fastify = require("fastify");
+const fastifySwagger = require("@fastify/swagger");
 const { itemRoutes } = require("./routes/v1/items");
 
-const build = (options = {}) => {
+const build = (options = {}, optionSwagger) => {
   const app = fastify(options);
+  app.register(fastifySwagger, optionSwagger);
   app.register(itemRoutes);
 
   return app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const { build } = require("./app.js");
 
-const app = build({ logger: true });
+const app = build(
+  { logger: true },
+  {
+    exposeRoute: true,
+    routePrefix: "/docs",
+    swagger: {
+      info: { title: "Fastify API test", version: "1.0.0" },
+    },
+  }
+);
 
 // Run
 app.listen(3000, function (err, address) {
