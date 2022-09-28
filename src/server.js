4347bfb2c692817be2e1cf408ba409275cc8d8f0
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 09:26:49 +0200

    added comment on server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
+//setup
 const fastify = require("fastify")({
   logger: true,
 });
 
+// Routes
 fastify.get("/", function (request, reply) {
   reply.send({ hello: "world" });
 });
 
+// Run
 fastify.listen(3000, function (err, address) {
   if (err) {
     fastify.log.error(err);
