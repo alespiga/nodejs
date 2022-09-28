00740985a4de5043878297c9b009d1ebc73596bb
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 16:06:57 +0200

    added web-app image on compose

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const fastifySwagger = require("@fastify/swagger");
 const fastifyPostgres = require("@fastify/postgres");
 const { itemRoutes } = require("./routes/v1/items");
 const { itemRoutes_v2 } = require("./routes/v2/items");
+const { healthcheck } = require("./routes/healthcheck");
 
 const build = (options = {}, optionSwagger = {}, optionPostgres = {}) => {
   const app = fastify(options);
@@ -15,6 +16,8 @@ const build = (options = {}, optionSwagger = {}, optionPostgres = {}) => {
   app.register(itemRoutes);
 
   app.register(itemRoutes_v2, { prefix: "/v2" });
+
+  app.register(healthcheck, { prefix: "/healthcheck" });
 
   return app;
 };
