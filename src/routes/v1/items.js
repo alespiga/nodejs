945d1ba0943f8a55a843265f996762cfcf1a4ab8
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 10:09:05 +0200

    added schema and reply send items

--- src/routes/v1/items.js
+++ src/routes/v1/items.js
@@ -1,8 +1,26 @@
 let items = require("../../data/itemsData");
 
+const getItemsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            id: { type: "integer" },
+            name: { type: "string" },
+            description: { type: "string" },
+          },
+        },
+      },
+    },
+  },
+};
+
 const itemRoutes = (fastify, options, done) => {
-  fastify.get("/", (request, reply) => {
-    return { hello: "world" };
+  fastify.get("/", getItemsOpts, (request, reply) => {
+    reply.send(items);
   });
 
   fastify.get("/:id", (request, reply) => {
