4d12a1c09371c8dba3e4e117b9db115b80a65527
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 09:58:48 +0200

    added items data &  use find method to match element in array

--- src/routes/v1/items.js
+++ src/routes/v1/items.js
@@ -1,8 +1,15 @@
+let items = require("../../data/itemsData");
+
 const itemRoutes = (fastify, options, done) => {
   fastify.get("/", (request, reply) => {
     return { hello: "world" };
   });
 
+  fastify.get("/:id", (request, reply) => {
+    const { id } = request.params;
+    const item = items.find((item) => item.id === id);
+    reply.send(item);
+  });
   done();
 };
 
