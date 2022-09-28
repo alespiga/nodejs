a85219c05d8fa9d04ac3faf5c2fedfa3ca673ecc
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 10:49:23 +0200

    added post request

--- src/routes/v1/items.js
+++ src/routes/v1/items.js
@@ -3,7 +3,7 @@ let items = require("../../data/itemsData");
 const item = {
   type: "object",
   properties: {
-    id: { type: "integer" },
+    id: { type: "string" },
     name: { type: "string" },
     description: { type: "string" },
   },
@@ -31,6 +31,22 @@ const getItemOpts = {
   },
 };
 
+const postItemOpts = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["name", "description"],
+      properties: {
+        name: { type: "string" },
+        description: { type: "string" },
+      },
+    },
+    response: {
+      201: item,
+    },
+  },
+};
+
 const itemRoutes = (fastify, options, done) => {
   fastify.get("/", getItemsOpts, (request, reply) => {
     reply.send(items);
@@ -41,6 +57,14 @@ const itemRoutes = (fastify, options, done) => {
     const item = items.find((item) => item.id === id);
     reply.send(item);
   });
+
+  fastify.post("/", postItemOpts, (request, reply) => {
+    const { name, description } = request.body;
+    const item = { id: String(items.lenght + 1), name, description };
+    items.push(item);
+    reply.code(201).send(item);
+  });
+
   done();
 };
 
