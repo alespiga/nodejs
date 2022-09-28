27fbc4a7b64e927bcdc07b76ef9f6011223004f2
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 10:14:52 +0200

    updated schema

--- src/routes/v1/items.js
+++ src/routes/v1/items.js
@@ -1,18 +1,31 @@
 let items = require("../../data/itemsData");
 
+const item = {
+  type: "object",
+  properties: {
+    id: { type: "integer" },
+    name: { type: "string" },
+    description: { type: "string" },
+  },
+};
+
 const getItemsOpts = {
   schema: {
     response: {
       200: {
         type: "array",
-        items: {
-          type: "object",
-          properties: {
-            id: { type: "integer" },
-            name: { type: "string" },
-            description: { type: "string" },
-          },
-        },
+        items: item,
+      },
+    },
+  },
+};
+
+const getItemOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: item,
       },
     },
   },
@@ -23,7 +36,7 @@ const itemRoutes = (fastify, options, done) => {
     reply.send(items);
   });
 
-  fastify.get("/:id", (request, reply) => {
+  fastify.get("/:id", getItemOpts, (request, reply) => {
     const { id } = request.params;
     const item = items.find((item) => item.id === id);
     reply.send(item);
