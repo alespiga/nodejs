c430306cccb8986e184d406df4dd398604e3f25a
Alessio Spigarelli <ale.spigarelli@mail.com>
2022-09-28 15:18:56 +0200

    added post test

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const fastify = require("fastify");
 const fastifySwagger = require("@fastify/swagger");
 const fastifyPostgres = require("@fastify/postgres");
 const { itemRoutes } = require("./routes/v1/items");
+const { itemRoutes_v2 } = require("./routes/v2/items");
 
 const build = (options = {}, optionSwagger = {}, optionPostgres = {}) => {
   const app = fastify(options);
@@ -12,6 +13,8 @@ const build = (options = {}, optionSwagger = {}, optionPostgres = {}) => {
   app.register(fastifySwagger, optionSwagger);
 
   app.register(itemRoutes);
+
+  app.register(itemRoutes_v2, { prefix: "/v2" });
 
   return app;
 };
---
+++ src/routes/v2/items.js
@@ -0,0 +1,44 @@
+const item = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+    description: { type: "string" },
+  },
+};
+const postItemOpts = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["name", "description"],
+      properties: {
+        name: { type: "string" },
+        description: { type: "string" },
+      },
+    },
+    response: {
+      201: item,
+    },
+  },
+};
+
+const itemRoutes_v2 = async (fastify, options, done) => {
+  fastify.post("/", postItemOpts, async (request, reply) => {
+    const client = await fastify.pg.connect();
+    try {
+      const { name, description } = request.body;
+      const { rows } = await fastify.pg.query(
+        "INSERT INTO items (name,description) VALUES ($1, $2) RETURNING *",
+        [name, description]
+      );
+      reply.code(201).send(rows[0]);
+    } catch (error) {
+      reply.send(error);
+    } finally {
+      client.release();
+    }
+  });
+  done();
+};
+
+module.exports = { itemRoutes_v2 };
